05ddb238fcc790376abe259ab6f0fe2b32aa327a
Marcus Rangel <webdevcampos@gmail.com>
2022-11-25 16:45:44 -0300

    Finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,15 @@
+const dice = document.querySelector(".dice");
 const adviceId = document.querySelector(".advice__id");
 const adviceText = document.querySelector(".advice__text");
-fetch("https://api.adviceslip.com/advice")
-  .then((res) => res.json())
-  .then((data) => {
-    adviceId.innerText = data.slip.id;
-    adviceText.innerText = `"${data.slip.advice}"`;
-  });
+const generateRandomSentence = () => {
+  fetch("https://api.adviceslip.com/advice")
+    .then((res) => res.json())
+    .then((data) => {
+      adviceId.innerText = data.slip.id;
+      adviceText.innerText = `'${data.slip.advice}'`;
+    });
+};
+
+dice.addEventListener("click", generateRandomSentence);
+
+window.onload = generateRandomSentence();
